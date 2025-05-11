6d8484b68a4598f7c07d8622edda464fd910dee3
raphaeltkorp <raphael@tkorp.com>
2025-05-12 00:04:39 +0200

    Update project URLs in the projects component for accuracy and consistency

--- components/ui/projects.tsx
+++ components/ui/projects.tsx
@@ -13,7 +13,7 @@ const projects = [
 			"L'application de gestion de tâches ultime. Développez vos compétences et obtenez des récompenses.",
 		imageUrl: "/images/projects/Melios.png",
 		category: "Mobile",
-		url: "https://melios.me",
+		url: "https://linktr.ee/Melios_app",
 		technologies: ["ReactNative", "Firebase", "Expo"],
 	},
 	{
@@ -23,6 +23,7 @@ const projects = [
 			"Une plateforme de création de portfolios pour les étudiants et les professionnels.",
 		imageUrl: "/images/projects/Penfolio.png",
 		category: "Web",
+		url: "https://portfolio-maker-five.vercel.app/",
 		technologies: ["NextJS", "TailwindCSS", "Vercel"],
 	},
 	{
@@ -32,6 +33,7 @@ const projects = [
 			"La plateforme de gestion pour les coachs. Développez votre activité et gérez vos clients.",
 		imageUrl: "/images/projects/Novacoach.png",
 		category: "Web",
+		url: "https://github.com/raphplt/novacoach",
 		technologies: ["NextJS", "ExpressJS", "Vercel"],
 	},
 	{
@@ -40,6 +42,7 @@ const projects = [
 		description: "La galerie de mes plus belles photos, triées par périodes.",
 		imageUrl: "/images/projects/Raphotos.png",
 		category: "Web",
+		url: "https://www.raphotos.fr/",
 		technologies: ["NextJS", "TailwindCSS", "Vercel"],
 	},
 	{
@@ -49,6 +52,7 @@ const projects = [
 			"Le site vitrine de Melios, qui contient un blog et des informations sur l'application.",
 		imageUrl: "/images/projects/Melios-Web.png",
 		category: "Web",
+		url: "https://melios.me",
 		technologies: ["NextJS", "TailwindCSS", "Vercel"],
 	},
 	{
@@ -58,6 +62,7 @@ const projects = [
 			"Une plateforme dédiée aux fans de Pokémon GO, avec des informations sur chaque Pokémon et un calculateur.",
 		imageUrl: "/images/projects/Pokélab.png",
 		category: "Web",
+		url: "https://pokelab-seven.vercel.app/",
 		technologies: ["NextJS", "TailwindCSS", "Vercel"],
 	},
 	{
@@ -67,6 +72,7 @@ const projects = [
 			"Un moteur de recherche Open Source et respectueux de la vie privée.",
 		imageUrl: "/images/projects/Myriade.png",
 		category: "Web",
+		url: "https://github.com/raphplt/myriade",
 		technologies: ["Python", "Svelte", "TypeScript"],
 	},
 	{
@@ -76,6 +82,7 @@ const projects = [
 			"Une plateforme de vérification de faits boostée à l'IA pour lutter contre la désinformation.",
 		imageUrl: "/images/projects/Verifact.png",
 		category: "Web",
+		url: "https://github.com/raphplt/verifact",
 		technologies: ["TypeScript", "OpenAI", "NextJS"],
 	},
 	{
@@ -85,6 +92,7 @@ const projects = [
 			"Une extension Chrome pour vous aider à rester concentré en bloquant les sites distrayants.",
 		imageUrl: "/images/projects/Clear-Mind.png",
 		category: "Extension",
+		url: "https://github.com/raphplt/clear-mind",
 		technologies: ["JavaScript", "Chrome Extension"],
 	},
 ];
