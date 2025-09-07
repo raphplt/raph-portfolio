0b317054217b8e2b273c5b0a1fa2cb0f7b32d916
raphaeltkorp <raphael@tkorp.com>
2025-09-07 17:27:31 +0200

    Refactor projects data structure and update ProjectsGrid component
    - Moved project and category data to a new file for better organization.
    - Updated ProjectsGrid component to import projects and categories from the new data file.
    - Added new projects: Quori, TCG Nexus, and Worth It, along with their respective images.

---
+++ lib/data/projects.ts
@@ -0,0 +1,124 @@
+export const projects = [
+	{
+		id: 1,
+		title: "Melios",
+		description:
+			"L'application de gestion de tâches ultime. Développez vos compétences et obtenez des récompenses.",
+		imageUrl: "/images/projects/Melios.png",
+		category: "Mobile",
+		url: "https://linktr.ee/Melios_app",
+		technologies: ["ReactNative", "Firebase", "Expo"],
+	},
+	{
+		id: 2,
+		title: "Quori",
+		description:
+			"Un assistant de publication LinkedIn pour développeurs et créateurs, qui transforme vos commits GitHub en posts engageants.",
+		imageUrl: "/images/projects/Quori.png",
+		category: "Web",
+		url: "https://www.quori.dev/",
+		technologies: ["NextJS", "NestJS", "Postgres", "Redis", "TypeScript"],
+	},
+	{
+		id: 3,
+		title: "TCG Nexus",
+		description:
+			"Une plateforme pour organiser des tournois, échanger des cartes et utiliser des outils stratégiques assistés par IA pour les jeux de cartes.",
+		imageUrl: "/images/projects/TCGNexus.png",
+		category: "Web",
+		url: "https://github.com/raphplt/tcg-nexus",
+		technologies: ["NextJS", "NestJS", "Postgres", "GraphQL", "TypeScript"],
+	},
+	{
+		id: 4,
+		title: "Penfolio",
+		description:
+			"Une plateforme de création de portfolios pour les étudiants et les professionnels.",
+		imageUrl: "/images/projects/Penfolio.png",
+		category: "Web",
+		url: "https://portfolio-maker-five.vercel.app/",
+		technologies: ["NextJS", "TailwindCSS", "Vercel"],
+	},
+	{
+		id: 5,
+		title: "Novacoach",
+		description:
+			"La plateforme de gestion pour les coachs. Développez votre activité et gérez vos clients.",
+		imageUrl: "/images/projects/Novacoach.png",
+		category: "Web",
+		url: "https://github.com/raphplt/novacoach",
+		technologies: ["NextJS", "ExpressJS", "Vercel"],
+	},
+	{
+		id: 6,
+		title: "Raphotos",
+		description: "La galerie de mes plus belles photos, triées par périodes.",
+		imageUrl: "/images/projects/Raphotos.png",
+		category: "Web",
+		url: "https://www.raphotos.fr/",
+		technologies: ["NextJS", "TailwindCSS", "Vercel"],
+	},
+	{
+		id: 7,
+		title: "Melios web",
+		description:
+			"Le site vitrine de Melios, qui contient un blog et des informations sur l'application.",
+		imageUrl: "/images/projects/Melios-Web.png",
+		category: "Web",
+		url: "https://melios.me",
+		technologies: ["NextJS", "TailwindCSS", "Vercel"],
+	},
+	{
+		id: 8,
+		title: "Pokélab",
+		description:
+			"Une plateforme dédiée aux fans de Pokémon GO, avec des informations sur chaque Pokémon et un calculateur.",
+		imageUrl: "/images/projects/Pokélab.png",
+		category: "Web",
+		url: "https://pokelab-seven.vercel.app/",
+		technologies: ["NextJS", "TailwindCSS", "Vercel"],
+	},
+	{
+		id: 9,
+		title: "Myriade",
+		description:
+			"Un moteur de recherche Open Source et respectueux de la vie privée.",
+		imageUrl: "/images/projects/Myriade.png",
+		category: "Web",
+		url: "https://github.com/raphplt/myriade",
+		technologies: ["Python", "Svelte", "TypeScript"],
+	},
+	{
+		id: 10,
+		title: "Verifact",
+		description:
+			"Une plateforme de vérification de faits boostée à l'IA pour lutter contre la désinformation.",
+		imageUrl: "/images/projects/Verifact.png",
+		category: "Web",
+		url: "https://github.com/raphplt/verifact",
+		technologies: ["TypeScript", "OpenAI", "NextJS"],
+	},
+	{
+		id: 11,
+		title: "Clear Mind",
+		description:
+			"Une extension Chrome pour vous aider à rester concentré en bloquant les sites distrayants.",
+		imageUrl: "/images/projects/Clear-Mind.png",
+		category: "Extension",
+		url: "https://github.com/raphplt/clear-mind",
+		technologies: ["JavaScript", "Chrome Extension"],
+	},
+
+	{
+		id: 13,
+		title: "Worth It",
+		description:
+			"Une application de planification et de priorisation pour mieux gérer vos projets et vos choix personnels.",
+		imageUrl: "/images/projects/WorthIt.png",
+		category: "Mobile",
+		url: "https://github.com/raphplt/worth-it",
+		technologies: ["ReactNative", "Expo"],
+	},
+];
+
+export const categories = ["All", "Web", "Mobile", "Extension"];
